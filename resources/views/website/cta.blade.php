b9d81b5d3e2ecec7bc7430944f4d6382f718909b
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2020-12-29 20:13:05 -0500

    updated cta

--- resources/views/website/cta.blade.php
+++ resources/views/website/cta.blade.php
@@ -6,7 +6,7 @@
                     <h2 class="text-white py-4 my-0">Our commitment is excellence</h2>
                 </div>
                 <div class="col-md-6 text-center">
-                    <a class="btn btn-danger btn-lg" type="button" href="{{ route('contact') }}">Request a Quote</a>
+                    <a class="btn btn-danger btn-lg" type="button" href="{{ route('contact') }}">Request a Consultation</a>
                 </div>
             </div>
         </div>
